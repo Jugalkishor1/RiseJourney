a553481521136f3f05ab5a158801a1a5ab687df9
Jugal Kishor <jugalpatel1054@gmail.com>
2025-09-10 22:32:56 +0530

    Define relationship4

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,6 @@
 class School < ApplicationRecord
   belongs_to :district
+  has_many :buses, dependent: :destroy
+  has_many :students, dependent: :destroy
+  has_many :users
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,3 @@
 class Student < ApplicationRecord
   belongs_to :school
-  belongs_to :parent
 end
